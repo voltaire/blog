af9c760eafb96420aabaee57264830ef3e8cef9e
Jon Chen <bsd@voltaire.sh>
2014-06-07 17:46:20 -0400

    no https for mc.voltaire.sh

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'bsdlp'
 SITENAME = u'voltaireMC'
-SITEURL = 'https://mc.voltaire.sh/'
+SITEURL = 'http://mc.voltaire.sh/'
 
 TIMEZONE = 'Etc/UTC'
 
